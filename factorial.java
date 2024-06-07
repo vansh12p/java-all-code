34fc708083cfc0d2ea96370e4173c097ac274631
vansh12p <147362683+vansh12p@users.noreply.github.com>
2024-06-08 00:48:34 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class factorial {
+    public static int factorial(int n){
+       int f=1;
+        for (int i=1;i<=n;i++){
+            f =f * i ;
+        }
+        return f;
+    }
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+
+    System.out.println(factorial(n));
+    
+}
+    
+
+    
+}
