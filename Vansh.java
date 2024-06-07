8cf0bf028db5318924705d92d280a57cb2477421
vansh12p <147362683+vansh12p@users.noreply.github.com>
2024-06-07 23:57:10 +0530

    Update Vansh.java

--- Vansh.java
+++ Vansh.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Vansh{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
-        System.out.println(a);
+        int b=sc.nextInt();
+        System.out.println(b);
     }
 }
