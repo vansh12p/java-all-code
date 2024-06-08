81758dc2dc385ac103511e5d67ad97378bbab232
vansh12p <147362683+vansh12p@users.noreply.github.com>
2024-06-08 18:22:38 +0530

    Add files via upload

---
+++ funoverloading.java
@@ -0,0 +1,30 @@
+// using parameter
+// public class funoverloading {
+//     public static int sum(int a,int b){
+//         return a+b;
+//     }
+//     public static int sum(int a,int b,int c){
+//         return a+b+c;
+//     }
+//     public static void main (String [] args){
+//         System.out.println(sum(3,5));
+//         System.out.println(sum(3,5,4));
+//     }
+    
+// }
+
+
+//using data type
+public class funoverloading {
+    public static int sum(int a,int b){
+        return a+b;
+    }
+    public static float sum(float a,float b,float c){
+        return a+b+c;
+    }
+    public static void main (String [] args){
+        System.out.println(sum(3,5));
+        System.out.println(sum(3.9f,5,4));
+    }
+    
+}
