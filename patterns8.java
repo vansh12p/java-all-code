4a551d02a69d813f7e8ea9aaffea3bf177a84f18
vansh12p <147362683+vansh12p@users.noreply.github.com>
2024-06-12 08:38:20 +0530

    Add files via upload

---
+++ patterns8.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class patterns8 {
+    public static void hollow_rhombus_pattern(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+
+            }
+            for(int j=1;j<=n;j++){
+            if(i==1 || i==n || j==1 || j==n ){
+                System.out.print("*");
+            }
+            else{
+                System.out.print(" ");
+            }
+        }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        hollow_rhombus_pattern(n);
+    }
+    
+}
