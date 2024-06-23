14101490d96a2cb50909ebfbd5cca687855ef9ac
vansh giri <147362683+vansh12p@users.noreply.github.com>
2024-06-23 20:15:43 +0530

    Update strings2.java

--- strings2.java
+++ strings2.java
@@ -1,7 +1,8 @@
 // to print array length
 public class strings2 {
+public static void main(String[]args){
     String fullname="vansh giri";
     System.out.print(fullname.length());
     
-    
+   } 
 }
